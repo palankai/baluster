72149c4d7467b0c9d5af8ac54fd8dbb0c2a3c5ca
Csaba Palankai <csaba.palankai@gmail.com>
2017-05-20 19:06:00 +0100

    Initial commit

---
+++ src/ns/base.py
@@ -0,0 +1,215 @@
+import asyncio
+import re
+from collections import defaultdict
+from contextlib import contextmanager
+from inspect import isclass, isawaitable
+from functools import wraps, partial
+
+
+class Maker:
+
+    def __init__(
+        self, func=None, *, cache=True, readonly=False, alias=None
+    ):
+        self._owner = None
+        self._name = None
+        self._cache = cache
+        self._readonly = readonly
+        self._alias = alias
+        self._hooks = []
+        if func is not None:
+            self(func)
+
+    def __call__(self, func):
+        self._func = func
+        return self
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        if asyncio.iscoroutinefunction(self._func):
+            return self._async_get(instance)
+        return self._get(instance)
+
+    def __set__(self, instance, value):
+        root = instance._root
+        name = instance._get_member_name(self._name)
+        if root._has(name):
+            raise AttributeError(
+                'The value `{name}` has already been set'.format(
+                    name=self._name
+                )
+            )
+        if self._readonly:
+            raise AttributeError(
+                'The value `{name}` is readonly'.format(
+                    name=self._name
+                )
+            )
+        root._set(name, value)
+
+    def __delete__(self, instance):
+        raise AttributeError('Attribute cannot be deleted')
+
+    def __set_name__(self, owner, name):
+        self._name = name
+        self._owner = owner
+
+    def _get(self, instance):
+        root = instance._root
+        name = instance._get_member_name(self._name)
+        if root._has(name):
+            return root._get(name)
+        value = self._func(instance, root)
+        if self._cache:
+            root._set(name, value)
+        return value
+
+    async def _async_get(self, instance):
+        root = instance._root
+        name = instance._get_member_name(self._name)
+        if root._has(name):
+            return root._get(name)
+        value = await self._func(instance, root)
+        if self._cache:
+            root._set(name, value)
+        return value
+
+    def hook(self, name):
+        def inner(f):
+            self._hooks.append((name, f))
+            return f
+        return inner
+
+    def setup(self, instance):
+        root = instance._root
+        if self._alias is not None:
+            if asyncio.iscoroutinefunction(self._func):
+                root._set_alias(
+                    self._alias,
+                    asyncio.coroutine(partial(self._async_get, instance))
+                )
+            else:
+                root._set_alias(self._alias, partial(self._get, instance))
+        for name, handler in self._hooks:
+            if asyncio.iscoroutinefunction(handler):
+                async def f(**kwargs):
+                    return await handler(instance, root, **kwargs)
+                root._add_handler(name, f)
+            else:
+                root._add_handler(name, partial(handler, instance, root))
+
+
+def make(func=None, **kwargs):
+    return Maker(func, **kwargs)
+
+
+class BaseNS:
+
+    def __init__(self, parent=None, name=None, _vars=None, _alias=None, _handlers=None):
+        self._parent = parent
+        if parent is not None:
+            self._root = parent._root
+            self._name = self._parent._get_member_name(name)
+        else:
+            self._root = self
+            self._name = None
+            self._vars = _vars or dict()
+            self._alias = _alias or dict()
+            self._handlers = _handlers or defaultdict(list)
+
+    def _join_name(self, *names):
+        return '.'.join(names)
+
+    def _get_member_name(self, name):
+        if self._name is None:
+            return name
+        return self._join_name(self._name, name)
+
+    def __getitem__(self, name):
+        maker = self._alias[name]
+        return maker()
+
+    def __iter__(self):
+        for alias in self._alias:
+            yield alias
+
+    def _set(self, name, value):
+        self._vars[name] = value
+
+    def _get(self, name):
+        return self._vars[name]
+
+    def _has(self, name):
+        return name in self._vars
+
+    def _set_alias(self, name, handler):
+        self._alias[name] = handler
+
+    def _add_handler(self, name, handler):
+        self._handlers[name].append(handler)
+
+    def __call__(self, name, **kwargs):
+        for handler in self._handlers[name]:
+            handler(**kwargs)
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        for handler in self._handlers['close']:
+            if asyncio.iscoroutinefunction(handler):
+                await handler()
+            else:
+                handler()
+
+    def copy(self, *names):
+        _alias = self._alias
+        _handlers = self._handlers
+        _vars = dict()
+        for key, value in self._vars.items():
+            for name in names:
+                print('name ?= key', name, key)
+                if re.match('^{}$'.format(name), key):
+                    _vars[key] = value
+        return self.__class__(
+            _alias=_alias, _handlers=_handlers, _vars=_vars
+        )
+
+
+class NSType(type):
+
+    def __new__(cls, name, bases, defined_members):
+        makers = []
+        nested = []
+        members = dict()
+
+        for k, v in defined_members.items():
+            if isinstance(v, Maker):
+                makers.append(v)
+            if isclass(v) and issubclass(v, BaseNS):
+                nested.append((k, v))
+            members[k] = v
+
+        members['_makers'] = tuple(makers)
+        members['_nested'] = tuple(nested)
+        return super().__new__(cls, name, bases, members)
+
+
+class NS(BaseNS, metaclass=NSType):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, nested in self._nested:
+            print(name, nested)
+            setattr(self, name, nested(parent=self, name=name))
+        for maker in self._makers:
+            maker.setup(self)
+
+
+@contextmanager
+def enter(ns, finish='close', **kwargs):
+    try:
+        yield ns
+    finally:
+        ns(finish, **kwargs)
---
+++ tests/test_ns.py
@@ -0,0 +1,128 @@
+import pytest
+
+from ns.base import NS, make, enter
+
+
+class CompositeRootCase(NS):
+
+    _value = 0
+    _closed = False
+
+    @make
+    def value(self, root):
+        return self._value
+
+    @make
+    def value_plus_100(self, root):
+        return self._value + 100
+
+    @value.hook('dec')
+    def _dec_value(self, root, amount=1):
+        self._value -= amount
+
+    @value.hook('close')
+    def _close(self, root):
+        self._closed = True
+
+    @make(cache=False)
+    def value_no_cache(self, root):
+        self._value += 1
+        return self._value
+
+    @make(readonly=True)
+    def value_readonly(self, root):
+        return self._value
+
+    @make(alias='alias')
+    def value_alias(self, root):
+        return 'as alias'
+
+
+class TestNS:
+
+    def test_sanity(self):
+        obj = CompositeRootCase()
+
+        assert obj.value == 0
+
+    def test_close(self):
+        obj = CompositeRootCase()
+
+        with enter(obj):
+            pass
+
+        assert obj._closed is True
+
+    def test_hooks(self):
+        obj = CompositeRootCase()
+
+        obj('dec')
+        assert obj._value == -1
+
+        obj('dec', amount=2)
+        assert obj._value == -3
+
+    def test_alias(self):
+        obj = CompositeRootCase()
+
+        assert obj.value_alias == 'as alias'
+        assert obj['alias'] == 'as alias'
+
+    def test_alias_access(self):
+        obj = CompositeRootCase()
+
+        assert list(obj) == ['alias']
+
+    def test_alias_if_set(self):
+        obj = CompositeRootCase()
+
+        obj.value_alias = 1
+        assert obj['alias'] == 1
+
+    def test_no_cahce(self):
+        obj = CompositeRootCase()
+
+        assert obj.value == 0
+        assert obj.value_no_cache == 1
+        assert obj.value_no_cache == 2
+        assert obj.value == 0
+
+    def test_set_new_value(self):
+        obj = CompositeRootCase()
+
+        obj.value = 3
+        assert obj.value == 3
+        assert obj.value_no_cache == 1
+        with pytest.raises(AttributeError):
+            obj.value = 5
+
+    def test_readonly(self):
+        obj = CompositeRootCase()
+
+        with pytest.raises(AttributeError):
+            obj.value_readonly = 3
+
+    def test_cannot_be_deleted(self):
+        obj = CompositeRootCase()
+
+        with pytest.raises(AttributeError):
+            del obj.value
+
+    def test_copy(self):
+        obj = CompositeRootCase()
+        obj.value = 3
+        assert obj.value == 3
+
+        copy = obj.copy()
+        assert copy.value == 0
+
+    def test_copy_keep_values(self):
+        obj = CompositeRootCase()
+        obj.value = 3
+        obj.value_plus_100 = 103
+        assert obj.value == 3
+        assert obj.value_plus_100 == 103
+
+        copy = obj.copy('value')
+        assert copy.value == 3
+        assert copy.value_plus_100 == 100
